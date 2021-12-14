e174b2c85886fa3398e901720895a3e649dbaeef
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-12-15 12:34:50 +1300

    update medusa

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -1,19 +1,18 @@
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000";
+const ADMIN_CORS = process.env.ADMIN_CORS;
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS;
 
 // Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // Medusa uses Redis, so this needs configuration as well
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL;
 
 // Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY;
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
 
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
