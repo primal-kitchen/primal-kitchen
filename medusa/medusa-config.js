4f2127895d08a95be16bb3da88ad5d45c37d3eed
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-12-20 17:14:34 +1300

    added strapi plugin for medusa

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -16,29 +16,38 @@ const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
 
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  // TODO: add strapi plugin ma boi
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
+	`medusa-fulfillment-manual`,
+	`medusa-payment-manual`,
+	// TODO: add strapi plugin ma boi
+	{
+		"resolve": `medusa-plugin-strapi`,
+		"options": {
+			"strapi_medusa_user": process.env.STRAPI_USERNAME,
+			"strapi_medusa_password": process.env.STRAPI_PASSWORD,
+			"strapi_url": process.env.STRAPI_URL,
+			"strapi_port": process.env.STRAPI_PORT,
+		},
+	},
+	// Uncomment to add Stripe support.
+	// You can create a Stripe account via: https://stripe.com
+	// {
+	//   resolve: `medusa-payment-stripe`,
+	//   options: {
+	//     api_key: STRIPE_API_KEY,
+	//     webhook_secret: STRIPE_WEBHOOK_SECRET,
+	//   },
+	// },
 ];
 
 module.exports = {
-  projectConfig: {
-    // TODO: activate redis ma boi
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    database_url: DATABASE_URL,
-    database_type: "postgres",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-  },
-  plugins,
+	projectConfig: {
+		// TODO: activate redis ma boi
+		// redis_url: REDIS_URL,
+		// For more production-like environment install PostgresQL
+		database_url: DATABASE_URL,
+		database_type: "postgres",
+		store_cors: STORE_CORS,
+		admin_cors: ADMIN_CORS,
+	},
+	plugins,
 };
