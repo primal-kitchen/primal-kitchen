e936af05e717709f7c4e5b35e802e1d15e39b21d
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2021-12-21 13:22:00 +1300

    test strapi and medusa data fetching on frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,42 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
-  return (
-      <main>changeed</main>
-  )
-}
+	const [asdfs, setAsdfs] = useState('nothing');
+	useEffect(() => {
+		fetch('https://strapi-admin.new.primalkitchen.nz.local/asdfs', {
+			// credentials: 'same-origin',
+		})
+			.then(res => res.json())
+			.then(res => res[0])
+			.then(res => setAsdfs(`${res.id} ${res.asdf} ${res.rt}`))
+			.catch(() => setAsdfs('error'));
+	}, [setAsdfs]);
 
-export default Home
+	const [medusaBS, setMedusaBS] = useState('nuk');
+	useEffect(() => {
+		fetch('https://medusa.new.primalkitchen.nz.local/store/products/prod_01FQD5F25J0MJTQ5GX8XJJFYSZ', {
+			// credentials: 'same-origin',
+		})
+			.then(res => res.json())
+			.then(res => setMedusaBS(`${res.product.id} ${res.product.title} ${res.product.description}`))
+			.catch(() => setMedusaBS('fucked out'));
+	}, [setMedusaBS]);
+
+	return (
+		<main>
+			<div>
+				<h1>from strapi</h1>
+				<p>{asdfs}</p>
+			</div>
+			<div>
+				<h1>from medusa</h1>
+				<p>{medusaBS}</p>
+			</div>
+		</main>
+	);
+};
+
+export default Home;
