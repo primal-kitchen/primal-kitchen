ddb87e6c5f773493d69fe56e4f83224e4c0ed347
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-02-01 19:11:53 +1030

    only build reverse proxy in setting up docker script

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker-compose --file docker-compose.setup.yaml --file docker-compose.yaml build
+docker-compose --file docker-compose.setup.yaml --file docker-compose.yaml build reverse-proxy
 docker-compose --file docker-compose.setup.yaml --file docker-compose.yaml --profile certificate-creation up
