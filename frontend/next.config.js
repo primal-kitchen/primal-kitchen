60fe667a07f28f8917a4385bcda4c9ad3cf7efd0
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2022-10-04 19:36:49 +1030

    fix hot reloading of frontend on file change when in docker & vagrant

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+	// fix webpack hot reloading on file change not working inside docker & vagrant
+	webpack: function (config, context) {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 }
